83afd37a936aed8d1798b9ab494994374d3177fe
Alicia Godefroy <alicia.godefroy@fk-agency.com>
2023-03-30 10:26:00 +0200

    test bullet

--- assets/script.js
+++ assets/script.js
@@ -28,11 +28,28 @@ function updateSlide() {
 
 updateSlide();
 
-// Gestion des flèches
+
+//----------- Gestion des dots
+
+// On récupère dans le DOM la div parent qui contient les dots
+let dots = document.getElementById('dots');
+
+//  On crée une boucle pour itérer dans le tableau slides et afficher le nombre de dots en fonction du nombre de slide
+for (let i = 0; i < slides.length; i++) {
+	let dots = document.getElementById('dots');
+	let dot = document.createElement("div");
+	dot.className = "dot dot"+i;
+	if ( i === 0 ){dot.className = "dot " + "dot"+i + " dot_selected";}
+	dots.appendChild(dot);
+}
+
+
+//----------- Gestion des flèches
 
 let arrowRight = document.querySelector('.arrow_right'); //On récupère la flèche dans le DOM
 arrowRight.addEventListener('click', function() { // On crée un événement au clic sur la flèche
   currentSlide++; // On incrémente +1 à la valeur de currentSlide pour passer à la slide suivante
+
   if (currentSlide >= slides.length) { // Si la slide active est >= à la longueur du tableau "slides" (donc si on arrive à la fin du tableau)
     currentSlide = 0; // On revient au début du tableau
   }
@@ -42,32 +59,9 @@ arrowRight.addEventListener('click', function() { // On crée un événement au 
 let arrowLeft = document.querySelector('.arrow_left');
 arrowLeft.addEventListener('click', function() {
   currentSlide--;
+
   if (currentSlide < 0) { // Si la slide active revient au début et est inférieure à 0 (le début du tableau)
     currentSlide = slides.length - 1; // On revient à la dernière slide du tableau 
   }
   updateSlide();
 });
-
-//------- Gestion des dots
-
-// On récupère dans le DOM la div parent qui contient les dots
-let dots = document.getElementById('dots');
-
-//  On crée une boucle pour itérer dans le tableau slides et afficher le nombre de dots en fonction du nombre de slide
-for (let i = 0; i < slides.length; i++) {
-	let dot = document.createElement("div");
-	dot.className = "dot";
-	dots.appendChild(dot);
-}
-
-
-
-// // Supprime la classe 'dot_selected' de tous les dots
-// let dotsList = dots.querySelectorAll('.dot');
-// dotsList.forEach((dot) => {
-//   dot.classList.remove('dot_selected');
-// });
-
-// // Ajoute la classe 'dot_selected' au dot correspondant à la slide active
-// let activeDot = dotsList[currentSlide];
-// activeDot.classList.add('dot_selected');
