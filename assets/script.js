b982a8d26ca7b3e6b16277c6c3b26468301066bc
Alicia Godefroy <alicia.godefroy@fk-agency.com>
2023-03-30 12:24:25 +0200

    ajout du dot_selected

--- assets/script.js
+++ assets/script.js
@@ -23,8 +23,8 @@ const slides = [
 for (let i = 0; i < slides.length; i++) {
 	let dots = document.getElementById('dots');
 	let dot = document.createElement("div");
-	dot.className = "dot";
-	if ( i === 0 ){dot.className = "dot" + " dot_selected";}
+	dot.className = "dot dot"+i;
+	if ( i === 0 ){dot.className = "dot " + "dot"+i + " dot_selected";}
 	dots.appendChild(dot);
 }
 
@@ -36,8 +36,8 @@ let currentSlide = 0;
 
 // On crée une fonction changeSlide pour changer l'image et le texte lorsqu'on appelle la fonction (au clic sur les flèches)
 function changeSlide() {
-  slideImg.src = `./assets/images/slideshow/${slides[currentSlide].image}`; // On change la valeur de l'attribut src dans l'image pour récupérer l'image de la slide en cours
-  slideTxt.innerHTML = slides[currentSlide].tagLine;  // On change la valeur du texte src pour récupérer la tagline de la slide en cours
+	slideImg.src = `./assets/images/slideshow/${slides[currentSlide].image}`; // On change la valeur de l'attribut src dans l'image pour récupérer l'image de la slide en cours
+	slideTxt.innerHTML = slides[currentSlide].tagLine;  // On change la valeur du texte src pour récupérer la tagline de la slide en cours
 }
 changeSlide();
 
@@ -46,20 +46,33 @@ changeSlide();
 
 let arrowRight = document.querySelector('.arrow_right'); //On récupère la flèche dans le DOM
 arrowRight.addEventListener('click', function() { // On crée un événement au clic sur la flèche
-  currentSlide++; // On incrémente +1 à la valeur de currentSlide pour passer à la slide suivante
+	currentSlide++; // On incrémente +1 à la valeur de currentSlide pour passer à la slide suivante
 
-  if (currentSlide >= slides.length) { // Si la slide active est >= à la longueur du tableau "slides" (donc si on arrive à la fin du tableau)
-    currentSlide = 0; // On revient au début du tableau
-  }
-  changeSlide(); // On appelle notre fonction changeSlide()
+	if (currentSlide >= slides.length) { // Si la slide active est >= à la longueur du tableau "slides" (donc si on arrive à la fin du tableau)
+		currentSlide = 0; // On revient au début du tableau
+	}
+
+	let dotSelected = document.querySelector('.dot_selected');
+	dotSelected.className = "dot dot"+(currentSlide-1);
+	if (currentSlide === 0) {dotSelected.className = "dot dot"+(slides.length-1);}
+	dotSelected = document.querySelector('.dot'+currentSlide);
+	dotSelected.className = "dot " + "dot"+ currentSlide + " dot_selected" ;
+
+	changeSlide(); // On appelle notre fonction changeSlide()
 });
 
 let arrowLeft = document.querySelector('.arrow_left');
 arrowLeft.addEventListener('click', function() {
-  currentSlide--;
+	currentSlide--;
 
-  if (currentSlide < 0) { // Si la slide active revient au début et est inférieure à 0 (le début du tableau)
-    currentSlide = slides.length - 1; // On revient à la dernière slide du tableau 
-  }
-  changeSlide();
+	if (currentSlide < 0) { // Si la slide active revient au début et est inférieure à 0 (le début du tableau)
+		currentSlide = slides.length - 1; // On revient à la dernière slide du tableau 
+	}
+
+	let dotSelected = document.querySelector('.dot_selected');
+	dotSelected.className = "dot dot"+(currentSlide+1);
+	if (currentSlide === (slides.length-1)) {dotSelected.className = "dot dot0";}
+	dotSelected = document.querySelector('.dot'+currentSlide);
+	dotSelected.className = "dot " + "dot"+ currentSlide + " dot_selected" ;
+	changeSlide();
 });
